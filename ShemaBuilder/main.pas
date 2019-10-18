1c60bb8709af262048f37cca66ab18cb938070cf
shishkin_angtel@rambler.ru <shishkinzel>
2019-10-18 17:10:54 +0300

    Добавил процедуру и функцию, вот что получилось.

--- ShemaBuilder/main.pas
+++ ShemaBuilder/main.pas
@@ -40,6 +40,8 @@ type
     { Private declarations }
     DeviceName : String;    // Имя устройства
     LS, RS  : TObjectList; //  Коллекция объектов TColodka для Левой и Правой сторон устройства
+    function differenceColodka(LObject : TObjectList): integer;
+   	procedure buildingColodka(const diffCol : Integer;LOject : TObjectList);
   public
     { Public declarations }
     // в этой процедуре должно происходить рисование
@@ -68,12 +70,12 @@ begin
   // Левая сторона
 
   LS := TObjectList.Create (true);
-
   C := TColodka.Create('Входы кнопок ПГУ');
   C.Contacts.Add(TContact.Create('K1',''));
   C.Contacts.Add(TContact.Create('K2','Подъезд'));
   C.Contacts.Add(TContact.Create('K3','Подъезд2'));
   C.Contacts.Add(TContact.Create('K4',''));
+  C.Contacts.Add(TContact.Create('K5',''));
   LS.Add(C);
 
   C := TColodka.Create('Входы динамиков ПГУ');
@@ -82,7 +84,22 @@ begin
   C.Contacts.Add(TContact.Create('Г3','Подъезд2'));
   C.Contacts.Add(TContact.Create('Г4',''));
   LS.Add(C);
+ //*******************************************
+  C := TColodka.Create('Входы динамиков ПГУ');
+  C.Contacts.Add(TContact.Create('Г1',''));
+  C.Contacts.Add(TContact.Create('Г2','Подъезд'));
+  C.Contacts.Add(TContact.Create('Г3','Подъезд2'));
+  C.Contacts.Add(TContact.Create('Г4',''));
+  LS.Add(C);
 
+  C := TColodka.Create('Входы кнопок ПГУ');
+  C.Contacts.Add(TContact.Create('K1',''));
+  C.Contacts.Add(TContact.Create('K2','Подъезд'));
+  C.Contacts.Add(TContact.Create('K3','Подъезд2'));
+  C.Contacts.Add(TContact.Create('K4',''));
+  C.Contacts.Add(TContact.Create('K5',''));
+  LS.Add(C);
+//**********************************************
 
   RS := TObjectList.Create (true);
   C := TColodka.Create('Тест');
@@ -96,7 +113,7 @@ end;
 
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
-  DeviceName := 'просто так себе рисунок';
+  DeviceName := 'K99';
 end;
 
 procedure TfrmMain.FormDestroy(Sender: TObject);
@@ -109,10 +126,11 @@ procedure TfrmMain.pb1Paint(Sender: TObject);
 var
  W : Integer;
  X : Integer;
+ differnceHeight : Integer;  // шаг между колодками
 begin
   // Чтобы Доска перерисовывалась нормально рисовать нужно в этом методе
   // Пример определения центра для вывода текста
-  //******************** Прорисовываем основную подложку *********************************************
+  //******************** Прорисовываем основную подложку **************************************
   with pb1.Canvas do
     begin
       Pen.Width := 1;
@@ -120,76 +138,26 @@ begin
       Pen.Style := psSolid;
       Rectangle(100,0,450,600);
     end;
-//**************************************************************************************************
+
 
 
   if DeviceName <> '' then
   begin
-     w := pb1.Canvas.TextWidth(DeviceName);
+     w := pb1.Canvas.TextWidth(DeviceName) +2;
      X := pb1.Width div 2 - W div 2;
      pb1.Canvas.TextOut(X,5,DeviceName);
   end;
-{
-//******************** Прорисовываем основную подложку *********************************************
-  with pb1.Canvas do
-    begin
-      Pen.Width := 1;
-      Pen.Color := clBlue;
-      Pen.Style := psSolid;
-      Rectangle(100,0,450,600);
-    end;
-//**************************************************************************************************
- }
-  if (LS = nil) or (RS =nil) then
 
+  if (LS = nil) or (RS =nil) then
   else
-  begin
-  var i : Integer := LS.Count;
-  var j : Integer := RS.Count;
-  var heightColodka : Integer := 0;
-   while (i > 0 ) or (j > 0 ) do
-     begin
-       if i > 0   then
-         begin
-       // линия
-//           pb1.Canvas.MoveTo(0,0);
-//           pb1.Canvas.LineTo(50,50);
-       // прямоуголник
-           with pb1.Canvas do
-             begin
-               Pen.Color := clBlack;
-               Pen.Style := psSolid;
-               Rectangle(100,100 + heightColodka,200,200 + heightColodka);
-               MoveTo(130,100 + heightColodka);
-               LineTo(130,200 + heightColodka);
-       // текст
-//               TextOut(50,50,'Это мой первый рисунок');
-             end;
-           Dec(i);
-           heightColodka := heightColodka +160;
-         end;
-       if j > 0   then
-         begin
-       // линия
-//           pb1.Canvas.MoveTo(10,10);
-//           pb1.Canvas.LineTo(10,110);
-       // прямоуголник
-           with pb1.Canvas do
-             begin
-               Pen.Color := clBlack;
-               Pen.Style := psSolid;
-               Rectangle(350,275,450,325);
-               MoveTo(420,275);
-               LineTo(420,325);
-             end;
-       // текст
-//           pb1.Canvas.TextOut(200,50,'Это мой первый рисунок');
-           Dec(j);
-         end;
-     end;
+    begin
+      differnceHeight := differenceColodka(LS);
+      buildingColodka(differnceHeight, LS);
+
+
+
     end;
 end;
-
 
 { TContact }
 
@@ -212,5 +180,71 @@ begin
   Contacts.Free;
   inherited;
 end;
-// Вопрос ? А где деструктор для TContact
+
+//********************************* Функция ******************************************************************
+function TfrmMain.differenceColodka(LObject : TObjectList): integer;
+  var
+    countColodka, countContact : Integer;
+    i : Integer;
+  const
+    heightContact = 15; // высота шрифта +2 пиксела
+    widthContact  = 20; // ширина 3 символов +2 пиксела
+    HeightWind = 600;
+    widthWind = 450;
+  begin
+  countColodka := LObject.Count;
+  countContact := 0;
+  i := 0;
+  while i <= countColodka - 1 do
+      begin
+        countContact := countContact + TColodka (LS.Items[i]).Contacts.Count;
+        Inc(i);
+      end;
+  Result := Round(((HeightWind - heightContact * countContact)/(countColodka +1)));
+  end;
+
+//********************************* Процедура *************************************************
+ procedure TfrmMain.buildingColodka(const diffCol : Integer;LOject : TObjectList);
+ var
+  yIndexBeginColodka, yIndexEndColodka : Integer;
+  countColodka : Integer;
+  yIndex : Integer;
+  differnceHeight : Integer;
+  i, j, k : Integer;
+
+ const
+   XBeginIndexContactLS = 100;  // Начальная координата контакта
+   XEndIndexContactLS = 125;    // Конечная  координата контакта
+   heightContact = 15; // высота шрифта +2 пиксела
+   widthContact  = 25; // ширина 3 символов +2 пиксела
+   HeightWind = 600;
+   widthWind = 450;
+ begin
+   differnceHeight := diffCol;
+   with pb1.Canvas do
+     begin
+       Pen.Color := clBlack;
+       Pen.Style := psSolid;
+     end;
+   countColodka := LS.Count;
+   i := 0;
+   yIndex := differnceHeight;
+   while i <= countColodka -1 do
+     begin
+       k := TColodka (LS.Items[i]).Contacts.Count -1;
+       yIndexBeginColodka := yIndex;
+       for j := 0 to k do
+         begin
+           pb1.Canvas.Rectangle(XBeginIndexContactLS, yIndex, XEndIndexContactLS, yIndex + heightContact);
+           pb1.Canvas.TextOut(XBeginIndexContactLS + 5, yIndex +1, TContact( TColodka(LS.Items[i]).Contacts.Items[j]).Contact);
+           yIndex := yIndex + heightContact;
+         end;
+      yIndexEndColodka := yIndex;
+      pb1.Canvas.Rectangle(XEndIndexContactLS, yIndexBeginColodka, XEndIndexContactLS +90, yIndexEndColodka);
+      pb1.Canvas.TextOut(XEndIndexContactLS + 5, yIndexBeginColodka + 3, TColodka(LS.Items[i]).Name);
+      yIndex := yIndexEndColodka + differnceHeight;
+      Inc(i);
+     end;
+
+ end;
 end.
