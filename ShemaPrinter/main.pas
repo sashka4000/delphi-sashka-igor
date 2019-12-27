c9e13706dac2c51a6d2cfb1256005628ecb39c84
shishkinzel <shishkin_angtel@rambler.ru>
2019-12-27 15:59:27 +0300

    Работа с FastReport

--- ShemaPrinter/main.pas
+++ ShemaPrinter/main.pas
@@ -16,6 +16,7 @@ type
     frxpdfxprt1: TfrxPDFExport;
     frxrtfxprt1: TfrxRTFExport;
     procedure btn1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -24,19 +25,45 @@ type
 
 var
   Form1: TForm1;
-
+  fPath : string;
 implementation
 
 {$R *.dfm}
 
 procedure TForm1.btn1Click(Sender: TObject);
-
+var
+  fOneList : Boolean;
 begin
-  frxrprt1.PrepareReport();
-  frxpdfxprt1.FileName := '1.pdf';
-  frxpdfxprt1.ShowDialog := true;
-  frxrprt1.Export(frxpdfxprt1);
+//frxrprt1.PrepareReport();
+if rg2.ItemIndex = 1 then   fOneList := False;
+if rg1.ItemIndex = 2 then
+   begin
+     frxrprt1.ShowReport();
+     frxpdfxprt1.FileName := 'allShemas.pdf';
+     frxpdfxprt1.ShowDialog := true;
+     frxrprt1.Export(frxpdfxprt1);
+   end
+else if rg1.ItemIndex = 1 then
+   begin
+     frxrprt1.ShowReport();
+     frxrtfxprt1.FileName := 'allShemas.rtf';
+     frxrtfxprt1.ShowDialog := true;
+     frxrprt1.Export(frxrtfxprt1);
+   end
+else
+  begin
+    frxrprt1.ShowReport();
+    frxrprt1.Print;
+  end;
 
+
+end;
+
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  fPath := ExtractFilePath(Application.ExeName) + 'shema\';
+ // frxrprt1.PrepareReport();
 end;
 
 end.
