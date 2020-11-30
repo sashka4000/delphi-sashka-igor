ef09c17c4cd1f0fc66071514930246aa835cfd50
shishkin_angtel@rambler.ru <shishkinzel>
2020-11-30 20:28:38 +0300

    Реализация исключительной ситуации raise

--- dbMonitoring/FParam.pas
+++ dbMonitoring/FParam.pas
@@ -51,16 +51,21 @@ var
   SelectedParametr: string;
 begin
   SelectedParametr := dbgrdPar.DataSource.DataSet.Fields[0].AsString;
-  // Запрос должен вывести график изменения параметра на указанном интервале
-  // для выбранного нами ранее пользователя
-  // вопрос 1. строится ли запрос по выбранному пользователю SelectedClientID?
-  // или как ?  решен
-  // вопрос 2. при указаннии интервала 01.11.2020 по 01.11.2020 - построится ли что-то?
-  // должно ли построиться по логике ? ожидания пользователя когда он хочет увидеть
-  // изменения параметра за 1 ноября ?
   DM_fireDAC.fdqry_Chart_Par.Active := False;
   DM_fireDAC.fdqry_Chart_Par.ParamByName('p').AsString := SelectedParametr;
   DM_fireDAC.fdqry_Chart_Par.ParamByName('p1').AsInteger := SelectedClientID;
+  if dtpBegin.DateTime > dtpEnd.DateTime then
+  begin
+    dtpBegin.Date := DateOf(SelectedClientRegDate);
+    dtpEnd.Date := DateOf(SelectedClientLastAccess);
+    dtpBegin.MaxDate := DateOf(SelectedClientLastAccess);
+    dtpBegin.MinDate := DateOf(SelectedClientRegDate);
+    dtpEnd.MaxDate := DateOf(SelectedClientLastAccess);
+    dtpEnd.MinDate := DateOf(SelectedClientRegDate);
+    raise  Exception.Create('Нарушен временной интервал');
+//    Exception.Create(MessageBox(Handle, PChar('Нарушен временной интервал'), PChar('Ошибка'), MB_ICONERROR + MB_OK))
+  end else
+  begin
   if DateOf(dtpBegin.DateTime) = DateOf(dtpEnd.DateTime) then
   begin
     DM_fireDAC.fdqry_Chart_Par.ParamByName('tbegin').AsDateTime := dtpBegin.DateTime;
@@ -74,6 +79,7 @@ begin
 
   DM_fireDAC.fdqry_Chart_Par.Active := True;
   dbchtParm.UndoZoom;
+  end;
 end;
 
 
@@ -95,7 +101,7 @@ begin
   dtpBegin.Date := DateOf(SelectedClientRegDate);
   dtpEnd.Date := DateOf(SelectedClientLastAccess);
   dtpBegin.MaxDate :=DateOf(SelectedClientLastAccess);
-  dtpBegin.MinDate := DateOf(SelectedClientRegDate) ; // Постоянно выдает ошибку выхода за минимальны предел
+  dtpBegin.MinDate := DateOf(SelectedClientRegDate) ;
   dtpEnd.MaxDate := DateOf(SelectedClientLastAccess);
   dtpEnd.MinDate := DateOf(SelectedClientRegDate) ;
 
