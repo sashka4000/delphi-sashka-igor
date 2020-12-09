6cadd924b83b56fad4d00aadd077de78b115d69e
shishkinzel <shishkin_angtel@rambler.ru>
2020-12-09 21:44:41 +0300

    Сделал парсер в dbgrid

--- MessageProcessor/main.pas
+++ MessageProcessor/main.pas
@@ -21,10 +21,15 @@ type
     btnDoText: TButton;
     btnDoValues: TButton;
     fdmtbl1: TFDMemTable;
-    ds1: TDataSource;
-    strngfldfdmtbl1Value1: TStringField;
-    strngfldfdmtbl1Value2: TStringField;
+    dsTb: TDataSource;
+    fdmtbl1Value: TStringField;
+    fdmtbl1ImageFile: TStringField;
+    fdmtbl1SoundFile: TStringField;
+    fdmtbl1AlertName: TStringField;
+    fdmtbl1Alert: TStringField;
+    fdmtbl1Commanf: TIntegerField;
     procedure FormCreate(Sender: TObject);
+    procedure btnDoValuesClick(Sender: TObject);
   private
     { Private declarations }
     function GetImageFolder: String;
@@ -34,14 +39,80 @@ type
     { Public declarations }
   end;
 
+
 var
   frmMain: TfrmMain;
 
 implementation
 
 {$R *.dfm}
+  uses
+  IdGlobal;
+
 
 { TfrmMain }
+
+
+
+procedure TfrmMain.btnDoValuesClick(Sender: TObject);
+var
+  tmp, tmp1, tmp2: string;
+  i, j: Integer;
+begin
+  tmp := '';
+  for i := 0 to mmo1.Lines.Count - 1 do
+  begin
+
+
+    dbgrd1.DataSource.DataSet.Edit;
+    tmp1 := mmo1.Lines.Strings[i];
+    tmp := Fetch(tmp1, '=');
+    dbgrd1.Fields[0].AsString := tmp;
+    tmp := Fetch(tmp1, ';');
+    dbgrd1.Fields[1].AsString := tmp;
+
+    tmp2 :=tmp1;
+    tmp := Fetch(tmp2, ':');
+    if tmp = ' S' then
+    begin
+      tmp := Fetch(tmp2, ';');
+      tmp1 :=tmp2;
+      dbgrd1.Fields[2].AsString := tmp;
+    end;
+
+    tmp2 := tmp1;
+    tmp := Fetch(tmp2, ':');
+    if tmp = ' N' then
+    begin
+      tmp := Fetch(tmp2, ';');
+      tmp1 :=tmp2;
+      dbgrd1.Fields[3].AsString := tmp;
+    end;
+
+    tmp2 := tmp1;
+    tmp := Fetch(tmp2, ':');
+    if tmp = ' A' then
+    begin
+      tmp := Fetch(tmp2, ';');
+      tmp1 :=tmp2;
+      dbgrd1.Fields[4].AsString := tmp;
+    end;
+
+    tmp2 := tmp1;
+    tmp := Fetch(tmp2, ':');
+    if tmp = ' C' then
+    begin
+      tmp := Fetch(tmp2, ';');
+      dbgrd1.Fields[5].AsString := tmp;
+
+    end;
+    dbgrd1.DataSource.DataSet.Append;
+//    fdmtbl1.Append;
+//    fdmtbl1.Fields[0].AsString := tmp;
+//    fdmtbl1.Post;
+  end;
+
+end;
 
 procedure TfrmMain.DBGrid1EditButtonClick(Sender: TObject);
 begin
@@ -50,8 +121,9 @@ end;
 
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
-  dbgrd1.Columns[0].ButtonStyle:=cbsEllipsis;
-  dbgrd1.Columns[1].ButtonStyle:=cbsEllipsis;
+
+
+
   dbgrd1.OnEditButtonClick:= DBGrid1EditButtonClick;
 end;
 
